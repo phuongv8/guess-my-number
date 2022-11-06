1c24c6013a90c8c0d5331d9b3cfc44e5ca118b9e
Phuong Vu <phuongvu@Phuongs-MacBook-Pro.local>
2022-11-05 20:13:17 -0700

    Implement game logic to check answer

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Click 'check' button, collect the input value
+// - if there is no value
+// - if there is wrong value:
+//    - if value is too high/too low
+//    - decrement score by 1
+//    - 'check' button change to 'retry'
+// - if there is correct value:
+//     - change ? to correct number
+//     - compare high score to current score, update high score
+//     - 'retry' btn change to new game
+//     - background color change to green, reset/new game btn red
+
+let number = document.querySelector(".number");
+let message = document.querySelector(".message");
+let score = document.querySelector(".score");
+let highscore = document.querySelector(".highscore");
+let guess = document.querySelector(".guess");
+let check = document.querySelector(".check");
+let initial_score = Number(score.textContent);
+
+const answer = Math.ceil(Math.random() * 20);
+console.log(answer);
+number.textContent = answer;
+
+function checkNumber() {
+  const guess_number = Number(guess.value);
+
+  if (!guess_number) {
+    message.textContent = "Invalid input!";
+  } else if (guess_number === answer) {
+    message.textContent = "Correct number!";
+  } else if (guess_number > answer) {
+    message.textContent = "Too high!";
+    score.textContent = initial_score - 1;
+  } else {
+    message.textContent = "Too low!";
+    score.textContent = initial_score - 1;
+  }
+}
+
+check.addEventListener("click", checkNumber);
